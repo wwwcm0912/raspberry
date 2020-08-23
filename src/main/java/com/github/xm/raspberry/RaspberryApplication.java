39a2da4a6a25dcdf4c34766db58e3650ec8cec0a
xiaomi <2324489934@qq.com>
2020-08-23 23:19:20 +0800

    init

---
+++ src/main/java/com/github/xm/raspberry/RaspberryApplication.java
@@ -0,0 +1,13 @@
+package com.github.xm.raspberry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspberryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaspberryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/xm/raspberry/RaspberryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.xm.raspberry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaspberryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
