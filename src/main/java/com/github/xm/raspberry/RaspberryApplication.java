001bf19551b9fa2f99d788360d09eaee3c4f1164
xiaomi <2324489934@qq.com>
2020-08-24 14:22:45 +0800

    update html name

--- src/main/java/com/github/xm/raspberry/RaspberryApplication.java
+++ src/main/java/com/github/xm/raspberry/RaspberryApplication.java
@@ -1,9 +1,11 @@
 package com.github.xm.raspberry;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.github.xm.raspberry.dao")
 public class RaspberryApplication {
 
     public static void main(String[] args) {
